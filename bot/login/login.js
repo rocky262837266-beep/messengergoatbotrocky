024b36cb6fade6032a384fb99a40a225ad925737
Ary <kawsermahmud404@gmail.com>
2026-08-03 14:54:29 +0600

    Update login.js

--- bot/login/login.js
+++ bot/login/login.js
@@ -1,5 +1,5 @@
 // set bash title
-process.stdout.write("\x1b]2;Goat Bot V2 - Made by NTKhang\x1b\x5c");
+process.stdout.write("\x1b]2;Goat Bot V2 - Made by NTKhang + Fix\x1b\x5c");
 const defaultRequire = require;
 
 function decode(text) {
